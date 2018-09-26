e17c5628206fa59a598b9b7c1cccf90e502b4524
Juha Lento <juha.lento@csc.fi>
2018-09-26 09:55:01 +0300

    Refactoring

---
+++ profile.d/common.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Prepends the given path to PATH environment variable and removes duplicates
+prependpath () {
+    PATH="$1:${PATH}:"
+    PATH="${PATH//:$1}"
+    PATH="${PATH%:}"
+}
+
+# Guessing where to find the most recent Emacs
+find_emacs () {
+    local emacspath=$(find $@ -name emacsclient -executable -printf "%h\n" 2> /dev/null | tail -1)
+    if [ -d ${emacspath} ]; then
+        export VISUAL="${emacspath}/emacsclient -c"
+        export ALTERNATE_EDITOR=""
+        alias emacs="$VISUAL"
+    fi
+}
+
+find_emacs $(dirname $(which emacsclient)) /usr/local/Cellar/emacs-plus/*/bin ${USERAPPL}/emacs/bin ${WRKDIR}/DONOTREMOVE/sisu-conda-envs/emacs/bin
+
