d3b04c72fec0a3f0fbcd7793e084f87b64b03962
Juha Lento <juha.lento@csc.fi>
2019-12-16 16:30:58 +0200

    Added "monitoring" functions

--- profile.d/puhti.sh
+++ profile.d/puhti.sh
@@ -46,3 +46,38 @@ workspaces () {
 }
 
 export -f project projappl scratch workspaces
+
+puhti-top-running () {
+    squeue -h -t R -o "%u %C" | \
+	awk '{
+                n[$1] += $2
+                if (length($1) > l) l = length($1)
+             }
+             END{
+                for (i in n) printf "%-*s%s\n", l + 2, i, n[i]
+             }' | \
+	sort -r -n -k 2
+}
+
+puhti-node-status () {
+    local p="fmi,fmitest"
+    printf "In partitions %s\n" "$p"
+    sinfo -h -p "$p" --Node -O nodelist,statelong | sort -u | \
+	awk '{
+                 n[$2]++;t++
+             }
+             END{
+                 print "    total    ", t
+                 for (i in n) printf "    %-10s%s\n", i, n[i]
+             }'
+    local p="gpu,gputest,hugemem,hugemem_longrun,large,longrun,small*,test"
+    printf "In partitions %s\n" "$p"
+    sinfo -h -p "$p" --Node -O nodelist,statelong | sort -u | \
+	awk '{
+                 n[$2]++;t++
+             }
+             END{
+                 print "    total    ", t
+                 for (i in n) printf "    %-10s%s\n", i, n[i]
+             }'
+}
